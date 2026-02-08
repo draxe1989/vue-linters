a12348affdfb86dbc12d2b611a36975b627f9559
Sergey <draxe1989@gmail.com>
2026-02-08 19:32:26 +0300

    prime

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,20 @@
-<script lang="ts" setup></script>
 <template>
-  <div>test</div>
+  <div>
+    <DataTable :value="products" table-style="min-width: 50rem">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="category" header="Category"></Column>
+      <Column field="quantity" header="Quantity"></Column>
+    </DataTable>
+  </div>
 </template>
+
+<script setup lang="ts">
+  import { DataTable, Column } from 'primevue'
+
+  const products = [
+    { code: 'f230fh1g3', name: 'P100', category: 'Office', quantity: 65 },
+    { code: 'f230fh1g4', name: 'P200', category: 'Office', quantity: 24 },
+    { code: 'f230fh1g5', name: 'P300', category: 'Office', quantity: 98 },
+  ]
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import '@/assets/css/main.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
+app.use(PrimeVue, {
+  theme: { preset: Aura },
+  options: {
+    darkModeSelector: '.dark', // Disables automatic dark mode
+
+    cssLayer: {
+      name: 'primevue',
+      order: 'theme, base, primevue',
+    },
+  },
+})
 app.mount('#app')
